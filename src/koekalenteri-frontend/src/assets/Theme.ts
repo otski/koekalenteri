164ca64911a580e873d1fb281daa7ad46be25983
Antti Olenius <antti.olenius@profitsoftware.com>
2021-04-20 10:28:42 +0300

    aole added test header component

---
+++ src/koekalenteri-frontend/src/assets/Theme.ts
@@ -0,0 +1,27 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#547463',
+            main: '#2a523d',
+            dark: '#1d392a',
+            contrastText: '#fff',
+          },
+          secondary: {
+            light: '#f7ebcf',
+            main: '#f6e6c4',
+            dark: '#aca189',
+            contrastText: '#000',
+          },
+        error: {
+          main: red.A400,
+        },
+        background: {
+          default: '#fff',
+        },
+    }, 
+})
+
+export default theme;
---
+++ src/koekalenteri-frontend/src/pages/SearchPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const SearchPage = () => {
+    return(
+        <Header></Header>
+    )
+}
+
+export default SearchPage;
