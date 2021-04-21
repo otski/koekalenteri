d57e857cd359c0eca958cc23d800e4bd50202c27
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-04-21 08:32:11 +0300

    Add lint step to CI, require indent of 2 spaces (#5)

--- src/koekalenteri-frontend/src/components/TestList/index.tsx
+++ src/koekalenteri-frontend/src/components/TestList/index.tsx
@@ -2,9 +2,9 @@ import { observer } from "mobx-react-lite"
 import { useStores } from '../../use-stores';
 
 const TestList = observer(() => {
-    const { testStore } = useStores();
+  const { testStore } = useStores();
 
-    return <ul>{testStore.tests.map(test => <li>{test.text}</li>)}</ul>
+  return <ul>{testStore.tests.map(test => <li>{test.text}</li>)}</ul>
 });
 
 export default TestList;
--- src/koekalenteri-frontend/src/stores/TestStore.ts
+++ src/koekalenteri-frontend/src/stores/TestStore.ts
@@ -1,17 +1,17 @@
 import { makeAutoObservable } from "mobx"
 
 export interface ITest {
-    id: number
-    text: string
+  id: number
+  text: string
 }
 
 export class TestStore {
-    public tests: ITest[] = [
-        { id: 1, text: 'test 1' },
-        { id: 2, text: 'test 2' }
-    ];
+  public tests: ITest[] = [
+    { id: 1, text: 'test 1' },
+    { id: 2, text: 'test 2' }
+  ];
 
-    constructor() {
-        makeAutoObservable(this)
-    }
+  constructor() {
+    makeAutoObservable(this)
+  }
 }
