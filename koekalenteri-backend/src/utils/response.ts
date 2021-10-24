788dd6a843bb5d9a5e56ee6ef4a1b1f8d1d03e74
kurkle <jukka.kurkela@gmail.com>
2021-10-24 18:43:22 +0300

    Remove /src from fe path, cors?

--- koekalenteri-backend/src/utils/response.ts
+++ koekalenteri-backend/src/utils/response.ts
@@ -1,7 +1,10 @@
 import { APIGatewayProxyResult } from "aws-lambda";
 
 export const response = (statusCode: number, body: any): APIGatewayProxyResult => ({
-    statusCode: statusCode,
-    body: JSON.stringify(body),
-    headers: {['Content-Type']: 'application/json'},
+  statusCode: statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+  },
 })
