1ffcaa3ac27cdd9e563e997ad5b5edcce80d0413
Jukka Kurkela <jukka.kurkela@gmail.com>
2022-09-06 23:03:20 +0300

    nix: Add missing official table creation (#123)

--- koekalenteri-backend/__test__/environment/local/create_tables.sh
+++ koekalenteri-backend/__test__/environment/local/create_tables.sh
@@ -19,6 +19,16 @@ aws dynamodb create-table \
 aws dynamodb batch-write-item --endpoint-url http://127.0.0.1:8000 --request-items file://judges.json
 
 aws dynamodb create-table \
+  --table-name official-table \
+  --attribute-definitions AttributeName=id,AttributeType=N \
+  --key-schema AttributeName=id,KeyType=HASH \
+  --billing-mode PAY_PER_REQUEST --endpoint-url http://127.0.0.1:8000
+
+aws dynamodb put-item --table-name official-table --item file://official1.json --endpoint-url http://127.0.0.1:8000
+aws dynamodb put-item --table-name official-table --item file://official2.json --endpoint-url http://127.0.0.1:8000
+aws dynamodb put-item --table-name official-table --item file://official3.json --endpoint-url http://127.0.0.1:8000
+
+aws dynamodb create-table \
   --table-name organizer-table \
   --attribute-definitions AttributeName=id,AttributeType=N \
   --key-schema AttributeName=id,KeyType=HASH \
